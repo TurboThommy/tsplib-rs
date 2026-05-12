c21d7337915ebb6ba86a6c254b129b5473382919
Thomas Klein <mail.thomas.klein@proton.me>
2026-05-12 23:20:28 +0200

    feat: add GET /algorithms endpoint

--- crates/tsplib-core/src/enums/algorithm_type.rs
+++ crates/tsplib-core/src/enums/algorithm_type.rs
@@ -1,6 +1,9 @@
 // ! This module contains the definition of the `AlgorithmType` enum, which represents the different types of algorithms that can be used to solve the TSP problem.
+use serde::{Deserialize, Serialize};
+use strum::EnumIter;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize, Deserialize, EnumIter)]
+#[serde(rename_all = "snake_case")]
 pub enum AlgorithmType {
     HeldKarp,
     Greedy,
--- crates/tsplib-server/src/main.rs
+++ crates/tsplib-server/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+//! REST API for the TSP library.
+
+use axum::{Json, Router, http::StatusCode, routing::get};
+use strum::IntoEnumIterator;
+use tsplib_core::enums::AlgorithmType;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(health_check))
+        .route("/algorithms", get(get_algorithms));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    println!("Listening on http://{}", listener.local_addr().unwrap());
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+/// Health check endpoint.
+async fn health_check() -> StatusCode {
+    StatusCode::OK
+}
+
+/// Get the list of available algorithms.
+async fn get_algorithms() -> Json<Vec<AlgorithmType>> {
+    Json(AlgorithmType::iter().collect())
 }
