5bd3739dc3f5b8fb91f10024fb44d939c34157d2
Thomas Klein <mail.thomas.klein@proton.me>
2026-05-12 22:51:32 +0200

    feat: add enum for available solver algorithms

---
+++ crates/tsplib-core/src/enums/algorithm_type.rs
@@ -0,0 +1,7 @@
+// ! This module contains the definition of the `AlgorithmType` enum, which represents the different types of algorithms that can be used to solve the TSP problem.
+
+#[derive(Debug)]
+pub enum AlgorithmType {
+    HeldKarp,
+    Greedy,
+}
